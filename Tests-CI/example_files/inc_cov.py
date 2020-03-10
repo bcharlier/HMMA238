c02ee4d847ea567c7548719ed61c747c1f1bdd12
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2020-03-10 22:43:55 +0100

    move example files to a subdir

---
+++ Tests-CI/example_files/inc_cov.py
@@ -0,0 +1,7 @@
+def inc(x):
+    if x < 0:
+        return 0
+    return x + 1
+
+def dec(x):
+     return x - 1
---
+++ Tests-CI/example_files/test_inc_cov.py
@@ -0,0 +1,5 @@
+from inc_cov import inc
+
+def test_inc():
+     assert inc(3) == 4
+
